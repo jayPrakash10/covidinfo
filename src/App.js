49f7480c632937b5771b0220e0adb609278c5fbf
jayPrakash10 <79151172+jayPrakash10@users.noreply.github.com>
2021-10-04 12:20:18 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,12 +1,36 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
-import Covid from './components/covid';
+import CovidDetails from './components/covid_info';
 import './css/covid.css'
 
 function App() {
+  
+  const [info, setInfo] = useState(null)
+  useEffect(()=>{
+    getapi()
+  },[])
+
+  function getapi(){
+    fetch("https://covid-193.p.rapidapi.com/statistics", {
+        "method": "GET",
+        "headers": {
+          "x-rapidapi-host": "covid-193.p.rapidapi.com",
+          "x-rapidapi-key": "9fac8ddff4msh6c51c9544932063p11bcebjsnc419840dc79a"
+        }
+      })
+      .then(response => 
+        response.json()
+      )
+      .then(data =>{
+        setInfo(data.response)
+      })
+      .catch(err => {
+        console.error(err);
+      });
+  }
   return (
     <div className="App">
-      <Covid />
+      <CovidDetails data={info} />
     </div>
   );
 }
