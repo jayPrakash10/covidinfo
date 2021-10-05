86cd4c787e185a717258a06e2804659f12236df9
jayPrakash10 <79151172+jayPrakash10@users.noreply.github.com>
2021-10-05 15:29:19 +0530

    Add files via upload

--- src/components/global_cases.js
+++ src/components/global_cases.js
@@ -1,9 +1,11 @@
 function Global({cases, recovered, deaths}){
     return(
             <div className='space'>
-                <div ><h4 className='case'>Total Cases</h4> <h2 className='case'>{cases}</h2></div>
-                <div className='text-success'><h4 className='case'>Recovered</h4> <h2 className='case'>{recovered}</h2></div>
-                <div className='text-danger'><h4 className='case'>Deaths</h4> <h2 className='case'>{deaths}</h2></div>
+                <div className='card-group'>
+                    <div className='card text-center list-group-item-secondary'><h4>Total Cases</h4> <h2>{cases}</h2></div>
+                    <div className=' card text-center list-group-item-success'><h4>Recovered</h4> <h2>{recovered}</h2></div>
+                    <div className='card text-center list-group-item-danger'><h4>Deaths</h4> <h2>{deaths}</h2></div>
+                </div>
             </div>
     )
 }
