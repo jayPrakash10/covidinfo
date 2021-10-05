1a3dac3be5026fb1398a471d5d9384f90636b7a2
jayPrakash10 <79151172+jayPrakash10@users.noreply.github.com>
2021-10-05 11:33:14 +0530

    Update global_cases.js

--- src/components/global_cases.js
+++ src/components/global_cases.js
@@ -2,8 +2,8 @@ function Global({cases, recovered, deaths}){
     return(
             <div className='space'>
                 <div ><h4 className='case'>Total Cases</h4> <h2 className='case'>{cases}</h2></div>
-                <div className='recover'><h4 className='case'>Recovered</h4> <h2 className='case'>{recovered}</h2></div>
-                <div ><h4 className='case'>Deaths</h4> <h2 className='case'>{deaths}</h2></div>
+                <div className='text-success'><h4 className='case'>Recovered</h4> <h2 className='case'>{recovered}</h2></div>
+                <div className='text-danger'><h4 className='case'>Deaths</h4> <h2 className='case'>{deaths}</h2></div>
             </div>
     )
 }
