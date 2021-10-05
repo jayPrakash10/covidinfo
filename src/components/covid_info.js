6cf73cd963f591037a83ffa14c7f05766f322fc6
jayPrakash10 <79151172+jayPrakash10@users.noreply.github.com>
2021-10-05 11:38:00 +0530

    Add files via upload

--- src/components/covid_info.js
+++ src/components/covid_info.js
@@ -1,46 +1,72 @@
 import React from "react"
 import Global from "./global_cases";
-// function sort_1M_pop(arr){
-//     for(let i=0; i<arr.length;i++)
-//         for(let j=0; j<arr.length-i-1; j++){
-//             if(arr[j].cases.total<arr[j+1].cases.total){
-//                 let temp = arr[j]
-//                 arr[j]=arr[j+1]
-//                 arr[j+1]=temp
-//             }
-//         }
-// }
+import Search from "./search_country";
+function sort_new(arr){
+    let j=0;
+    let i=0
+    while(i<arr.length){
+        if(arr[i].cases.new!=null){
+            var temp=arr[i]
+            i=i-1
+            while(i>=0 && arr[i].cases.new==null){
+                arr[i+1]=arr[i]
+                i=i-1
+            }
+            i=i+1;
+            arr[i]=temp
+            i=j;
+        }
+        i++;
+        j++;
+    }
+
+    for(let i=0; i<arr.length;i++)
+        for(let j=0; j<arr.length-i-1; j++){
+            if(parseInt(arr[j].cases.new)<parseInt(arr[j+1].cases.new)){
+                let temp = arr[j]
+                arr[j]=arr[j+1]
+                arr[j+1]=temp
+            }
+        }
+}
 
 function CovidDetails({data}){
-    let details = []
+    let details =[]
     let total_cases = 0
     let total_recovered = 0
     let total_deaths = 0
-    // sort_1M_pop(data)
+    sort_new(data)
+    
+    function show(){
+        data.forEach(element => {
+            if(element.country=='All'){
+                total_cases=element.cases.total
+                total_recovered=element.cases.recovered
+                total_deaths=element.deaths.total
+            }
+            else if(element.country!='North-America' && element.country!='South-America' && element.country!='Europe' && element.country!='Asia' && element.country!='Africa'){
+                const country = React.createElement('td',{},element.country)
+                const totalCases = React.createElement('td',{},element.cases.total)
+                const totalRecovered = React.createElement('td',{className:'text-success'},element.cases.recovered)
+                const newCases = React.createElement('td',{className:'text-primary'},element.cases.new)
+                const activeCases = React.createElement('td',{},element.cases.active)
+                const criticalCases = React.createElement('td',{className:'text-warning'},element.cases.critical)
+                const deaths = React.createElement('td',{className:'text-danger'},element.deaths.total)
+    
+                const row = React.createElement('tr',{},[country,totalCases,totalRecovered,newCases,activeCases,criticalCases,deaths])
+                details.push(row)
+            }        
+        });
+    }
 
-    data.forEach(element => {
-        if(element.country=='All')
-        {
-        total_cases=element.cases.total
-        total_recovered=element.cases.recovered
-        total_deaths=element.deaths.total
-        }
-        else if(element.country!='North-America' && element.country!='South-America' && element.country!='Europe' && element.country!='Asia' && element.country!='Africa'){
-            const country = React.createElement('td',{},element.country)
-            const totalCases = React.createElement('td',{},element.cases.total)
-            const totalRecovered = React.createElement('td',{},element.cases.recovered)
-            const newCases = React.createElement('td',{},element.cases.new)
-            const activeCases = React.createElement('td',{},element.cases.active)
-            const criticalCases = React.createElement('td',{},element.cases.critical)
-            const deaths = React.createElement('td',{},element.deaths.total)
-            const row = React.createElement('tr',{},[country,totalCases,totalRecovered,newCases,activeCases,criticalCases,deaths])
-            details.push(row)
-        }        
-    });
+    show()
     
     return(
         <div className='container'>
             <Global cases={total_cases} recovered={total_recovered} deaths={total_deaths}/>
+            
+            <Search data={data}/>
+
             <div className='space'>
                 <table className='table table-striped'>
                     <thead>
---
+++ src/components/search_country.js
@@ -0,0 +1,64 @@
+import { useState } from "react";
+
+function Search({data}){
+    var search = document.getElementById('searchByCountry')
+    const [cases, setcases] = useState({totalCases:0, recover:0, newCases:0, activeCases:0, criticalCases:0, deaths:0})
+    var err = true
+    function show(){
+        let country = document.getElementById('country').value
+        data.forEach(element => {
+            if(country.toLowerCase()==element.country.toLowerCase()){
+                setcases({totalCases : element.cases.total,
+                recover : element.cases.recovered,
+                newCases : element.cases.new,
+                activeCases : element.cases.active,
+                criticalCases : element.cases.critical,
+                deaths : element.deaths.total})
+                search.style.display = 'block'
+                err=false
+            }
+        });
+        if(err){
+            document.getElementById('error').style.display='block';
+            search.style.display='none'
+        }
+    }
+
+    return(
+        <>
+            <div class="input-group mb-3">
+                <input type="text" class="form-control" placeholder="Search for Country or Continent" id='country' onChange={()=>{document.getElementById('error').style.display = 'none'}}/>
+                <button class="btn btn-outline-secondary" type="button" id="button-addon2" onClick={()=> show()}><i className='fa fa-search'></i></button>
+            </div>
+            <div id='error' className='error text-danger'>
+                Invalid Country or continent name
+            </div>
+            <div id='searchByCountry'>
+                <table className='table'>
+                    <thead>
+                        <tr>
+                            <th scope="col">Total Cases</th>
+                            <th scope="col">Recovered</th>
+                            <th scope="col">New Cases</th>
+                            <th scope="col">Active Cases</th>
+                            <th scope="col">Critical Cases</th>
+                            <th scope="col">Deaths</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>{cases.totalCases}</td>
+                            <td>{cases.recover}</td>
+                            <td>{cases.newCases}</td>
+                            <td>{cases.activeCases}</td>
+                            <td>{cases.criticalCases}</td>
+                            <td>{cases.deaths}</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </>
+    )
+}
+
+export default Search
